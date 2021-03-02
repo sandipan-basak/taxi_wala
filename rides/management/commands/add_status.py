d83460c169b834e6e6f09d12216e999cb3b019cf
Sandipan Basak <sandipan329@gmail.com>
2021-03-03 02:59:08 +0530

    initial status

--- rides/management/commands/add_status.py
+++ rides/management/commands/add_status.py
@@ -1,12 +1,15 @@
-from django.core.management.base import NoArgsCommand, make_option
+from django.core.management.base import BaseCommand
+from rides.models import Status
 
-class Command(NoArgsCommand):
+class Command(BaseCommand):
 
-    help = "Whatever you want to print here"
+    # help = "Whatever you want to print here"
 
-    option_list = NoArgsCommand.option_list + (
-        make_option('--verbose', action='store_true'),
-    )
-
-    def handle_noargs(self, **options):
-        ... call your script here ...
+    
+    def handle(self, **options):
+        Status.objects.all().delete()
+        Status.objects.create(name='On Queue', color='#007bff')
+        Status.objects.create(name='Ongoing', color='#28a745')
+        Status.objects.create(name='Completed', color='#28a745')
+        Status.objects.create(name='Cancelled', color='#00000')
+        print(Status.objects.all())
