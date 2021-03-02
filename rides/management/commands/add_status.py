62c3e0b42e707c17cd7d0c7538047887d6027884
Guddu327 <sandipan329@gmail.com>
2021-03-02 17:47:09 +0530

    custom commands

---
+++ rides/management/commands/add_status.py
@@ -0,0 +1,12 @@
+from django.core.management.base import NoArgsCommand, make_option
+
+class Command(NoArgsCommand):
+
+    help = "Whatever you want to print here"
+
+    option_list = NoArgsCommand.option_list + (
+        make_option('--verbose', action='store_true'),
+    )
+
+    def handle_noargs(self, **options):
+        ... call your script here ...
--- scripts/createStatus.py
+++ scripts/createStatus.py
@@ -1,19 +1,9 @@
-import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'uber_clone.settings')
-
-import django
-django.setup()
-
 from rides.models import Status
-
 
 def create_objects(name, color):
     Status.objects.create(name, color)
 
-
-
-
-if '__name__' == '__main__':
+def run():
     Status.objects.all().delete()
     s1 = create_objects('On Queue', '#007bff')
     s2 = create_objects('Ongoing', '#28a745')
